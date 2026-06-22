937dba2b63210566ba8a506c4e604df9264fc73b
shubranshumishra22 <shubranshumishra22@gmail.com>
2026-06-22 20:31:04 +0530

    fix: remove Sentry (not configured), clean up config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@nudge/ui', '@nudge/db', '@nudge/ai'],
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
@@ -10,10 +9,4 @@ const nextConfig = {
   },
 }
 
-const sentryWebpackPluginOptions = {
-  silent: true,
-  hideSourceMaps: true,
-}
-
-const { withSentryConfig } = require('@sentry/nextjs')
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+module.exports = nextConfig
